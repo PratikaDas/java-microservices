441c3345db37e9862f5e57025dd5ee5e490602fa
PratikaDas <saipratika.das@gmail.com>
2018-02-13 14:55:04 +0530

    first commit

---
+++ src/main/java/com/springboot/starter/CourseAPI.java
@@ -0,0 +1,14 @@
+package com.springboot.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.autoconfigure.web.servlet.*;
+import org.springframework.boot.SpringBootConfiguration;
+
+public class CourseAPI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(CourseAPI.class, args);
+	}
+
+}
